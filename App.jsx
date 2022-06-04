6bd401fc28b814b8a72c64fcaee9023636be349d
yosuke <dalukid-r10y@i.softbank.jp>
2022-06-04 14:52:59 +0900

    Create MemoDetailScreen

--- App.jsx
+++ App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MemoListScreen from './src/screens/MemoLIstScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
+    <MemoDetailScreen />
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-
-});
