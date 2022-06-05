4cbcae0ecc9d4990fd8f9af1644ede581eaa54b8
yosuke <dalukid-r10y@i.softbank.jp>
2022-06-05 15:02:44 +0900

    Create LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 // import MemoListScreen from './src/screens/MemoLIstScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
-
+    <SignUpScreen />
   );
 }
 
