454c2c1956fb1c6287328991ed5136cb489baf68
yosuke <dalukid-r10y@i.softbank.jp>
2022-06-05 13:56:32 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoLIstScreen';
+// import MemoListScreen from './src/screens/MemoLIstScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
 
   );
 }
+
+//* eslint linebreak-style: ["error", "windows"] *//
