720969afad97c75f849e58c6598254ea6b55e7f3
yosuke <dalukid-r10y@i.softbank.jp>
2022-06-19 16:49:38 +0900

    Fetch data from firestore and pass it to FlatList

--- src/screens/MemoLIstScreen.jsx
+++ src/screens/MemoLIstScreen.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Alert } from 'react-native';
+import firebase from 'firebase';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
@@ -7,6 +8,7 @@ import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  const [memos, setMemos] = useState([]);
   useEffect(() => {
     navigation.setOptions({
       /* eslint-disable-next-line */
@@ -14,9 +16,35 @@ export default function MemoListScreen(props) {
     });
   }, {});
 
+  useEffect(() => {
+    const db = firebase.firebase();
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => {};
+    if (currentUser) {
+      const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
+      unsubscribe = ref.onSnapshot((snapshot) => {
+        const userMemos = [];
+        snapshot.forEach((doc) => {
+          console.log(doc.id, doc.data()); // eslint-disable-line no-console
+          const data = doc.data();
+          userMemos.push({
+            id: doc.id,
+            bodyText: data.bodyText,
+            updatedAt: data.updatedAt.toDate(),
+          });
+        });
+        setMemos(userMemos);
+      }, (error) => {
+        console.log(error); // eslint-disable-line no-console
+        Alert.alert('データの読み込みに失敗しました');
+      });
+    }
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MemoList />
+      <MemoList memos={memos} />
       <CircleButton
         name="plus"
         onPress={() => { navigation.navigate('MemoCreate'); }}
